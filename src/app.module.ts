234e296fd965f03ddc5d5f677b2a7353b5d22e60
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2023-01-31 21:29:04 -0300

    feat: add sqlite config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RegisterClientModule } from './RegisterClient/registerClient.module';
 
 @Module({
-  imports: [RegisterClientModule],
+  imports: [
+    RegisterClientModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
